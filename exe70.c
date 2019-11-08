6e57c1f56b094e5d58cf24e796dff3fc83bc3d22
Voulth <mateusp2oliveira@hotmail.com>
2019-11-08 20:22:12 +0000

    Erro Floating point Reduce

--- exe70.c
+++ exe70.c
@@ -17,24 +17,45 @@ signicativa em guardar-se o sinal separadamente ou isso não tem importância? 
 
 
 struct fraction{
-float num, den;
+int num, den;
 
 };
 
 float  mult(struct fraction *a, struct fraction *b){
-    return  (a->num/a->den)*(b->num/b->den);
+    float A,B,A1,B1;
+    A=a->den;
+    A1=a->num;
+    B=b->den;
+    B1=b->num;
+
+    return  (A1/A)*(B1/B);
 
 }
 
 float add(struct fraction *a, struct fraction *b){
-    return (a->num/a->den)+(b->num/b->den);
+    float A,B,A1,B1;
+    A=a->den;
+    A1=a->num;
+    B=b->den;
+    B1=b->num;
 
-
+    return (A1/A)+(B1/B);
 }
 
-void reduce(struct fraction a){
+void reduce(struct fraction *a){
+//Fazer MDC
+int temp, orNUM, orDEN;
+orNUM = a->num;
+orDEN = a->den;
+while(a->num != 0){
+    temp = a->den;
+    a->den = a->num;
+    a->num = temp % a->num;
+}
+int div = a->num;
 
-
+a->num = orNUM / div;
+a->den = orDEN / div;
 }
 
 int main(void){
@@ -45,4 +66,7 @@ b.num=1;
 b.den=2;
 printf("%f\n",mult(&a,&b));
 printf("%f\n",add(&a,&b));
+
+reduce(&a);
+printf("Num: %d Den: %d n",a.num,a.den);
 }
