75c26db7c4cb386d9ed3ba1014c79dea2f912043
Voulth <55609484+Voulth@users.noreply.github.com>
2019-11-15 22:47:05 -0300

    Faz o que é preciso, mas pode ser mais otimizado

--- exe72.c
+++ exe72.c
@@ -6,29 +6,38 @@ struct data{
     int dia, mes, ano;
 
 };
-//Começar recebendo duas structs depois mudar para receber uma só e por ponteiros e arrumar o esquema dos IF's 
+//Começar recebendo duas structs depois mudar para receber uma só e por ponteiros e arrumar o esquema dos IF's para ficar mais dinamico 
 void DataAntiga(struct data b, struct data c){
     if(b.ano<c.ano){
-        if(b.mes<c.mes){
-            if(b.dia<c.dia) printf("A data: %d/%d/%d\n é mais antiga",b.dia,b.mes,b.ano);
-            else printf("A data: %d/%d/%d\n é mais antiga",c.dia,c.mes,c.ano);
-        }
-        else{
-             printf("A data: %d/%d/%d\n é mais antiga",c.dia,c.mes,c.ano);
-             }
+        printf("A Data %d/%d/%d é mais antiga\n",b.dia,b.mes,b.ano);
     }
     else{
-             printf("A data: %d/%d/%d\n é mais antiga",c.dia,c.mes,c.ano);
-        }
+            if(b.mes<c.mes){
+                 printf("A data: %d/%d/%d e mais antiga\n",b.dia,b.mes,b.ano);
 
+            }
+            else{
+                if(b.dia<c.dia){
+                    printf("A data: %d/%d/%d e mais antiga\n",b.dia,b.mes,b.ano);
+                }
+                else{
+                printf("A data: %d/%d/%d e mais antiga\n",c.dia,c.mes,c.ano);
+                }
+
+            }
+        }
+    if(b.ano==c.ano && b.mes==c.mes && b.dia == c.dia) printf("As Datas são iguais\n");
 
 }
 
 
 
+
 int main(){
     
-    
-
+    struct data d={11,12,2017};
+    struct data a={11,12,2017};
+    printf("%d/%d/%d\n",d.dia,d.mes,d.ano);
+    DataAntiga(d,a);
 
 }
