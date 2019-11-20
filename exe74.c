15bf41dd833cd76271abb58643b4c83ea8f8a475
Voulth <mateusp2oliveira@hotmail.com>
2019-11-20 14:32:40 +0000

    Segmentation Fault erro no Peri

--- exe74.c
+++ exe74.c
@@ -1,30 +1,46 @@
 /*7.4 Crie um registro que representa os dados de um terreno de forma retangular. Depois de
 escolher como representar o terreno, declare um vetor de 100 terrenos e apresente algoritmos
 para:
-(a) Listar os terrenos que são quadrados.
+(a) Listar os terrenos que são quadrados. OK
 (b) Encontrar o terreno de maior perímetro.
-(c) Encontrar o terreno que mais se assemelha a um quadrado.
+(c) Encontrar o terreno que mais se assemelha a um quadrado. +- Pronto
 (d) Encontrar o terreno que menos se assemelha a um quadrado.
 */
 #include <stdio.h>
 
-
+int *peri = 0;
 
 struct terreno{//Terreno tem base e altura e angulos internos
-int base, altura;
+int base, altura, angulo;
 
 };
 
-void pegaTerreno(struct terreno *t){
-printf("Insira as caracteristicas de um terreno em metros: Altura e Base \n");
-scanf("%d %d",&t->altura,&t->base);
-}
-
-int TerrenoQuadrado(struct terreno *t){
-    if(t->altura == t->base) return 1;
+int perimetro(struct terreno *t){
+    int per;
+    per = 2*(t->base+t->altura);
+    if(per>=*peri) *peri = per;
 
     return 0;
 }
+
+void pegaTerreno(struct terreno *t){
+printf("Insira as caracteristicas de um terreno em metros: Altura, Base e angulo\n");
+scanf("%d %d %d",&t->altura,&t->base, &t->angulo);
+}
+
+int TerrenoQuadeRet(struct terreno *t){
+    if(t->altura == t->base && t->angulo == 90){
+         printf("%d  %d  %d é quadrado\n",t->altura,t->base, t->angulo);
+         perimetro(t);// Se já disse que a função iria receber um ponteiro eu não preciso dizer que ela é um ponteiro de novo 
+         }
+    if(t->altura != t->base && t->angulo == 90){
+         printf("%d  %d  %d é um retangulo\n",t->altura,t->base, t->angulo);
+         perimetro(t);// Se já disse que a função iria receber um ponteiro eu não preciso dizer que ela é um ponteiro de novo 
+         }
+   return 0;
+}
+
+
 
 void main(){
 
@@ -35,9 +51,10 @@ for(int i=0;i<=4;i++){
 }
 
 for(int j=0;j<=4;j++){
-
-    if(TerrenoQuadrado(&t[j])==1) printf("%d e %d é quadrado\n",t[j].altura,t[j].base);
+    TerrenoQuadeRet(&t[j]); 
 }
 
+
+printf("%ls",peri);
 
 }
