43bb51497ff2be9abf37335399ba6860408be522
Voulth <mateusp2oliveira@hotmail.com>
2019-11-20 18:32:11 +0000

    Arrumar alocação vetor, repetição do que é impresso e arrumar c e d

--- exe74.c
+++ exe74.c
@@ -7,8 +7,9 @@ para:
 (d) Encontrar o terreno que menos se assemelha a um quadrado.
 */
 #include <stdio.h>
+#include <stdlib.h>
 
-int *peri = 0;
+int peri = 0, bas=0, alt=0;
 
 struct terreno{//Terreno tem base e altura e angulos internos
 int base, altura, angulo;
@@ -16,10 +17,12 @@ int base, altura, angulo;
 };
 
 int perimetro(struct terreno *t){
-    int per;
-    per = 2*(t->base+t->altura);
-    if(per>=*peri) *peri = per;
-
+    int *per;
+    *per = 2*(t->base+t->altura);
+    if(*per>=peri){ 
+        peri = *per;
+        return 1;
+    }
     return 0;
 }
 
@@ -31,30 +34,55 @@ scanf("%d %d %d",&t->altura,&t->base, &t->angulo);
 int TerrenoQuadeRet(struct terreno *t){
     if(t->altura == t->base && t->angulo == 90){
          printf("%d  %d  %d é quadrado\n",t->altura,t->base, t->angulo);
-         perimetro(t);// Se já disse que a função iria receber um ponteiro eu não preciso dizer que ela é um ponteiro de novo 
+         if(perimetro(t)==1){
+             bas = t->base;
+             alt = t->altura;
+             
+            }// Se já disse que a função iria receber um ponteiro eu não preciso dizer que ela é um ponteiro de novo 
+         return 2;   
          }
     if(t->altura != t->base && t->angulo == 90){
          printf("%d  %d  %d é um retangulo\n",t->altura,t->base, t->angulo);
-         perimetro(t);// Se já disse que a função iria receber um ponteiro eu não preciso dizer que ela é um ponteiro de novo 
+         if(perimetro(t)==1){
+             bas = t->base;
+             alt = t->altura;
+            }// Se já disse que a função iria receber um ponteiro eu não preciso dizer que ela é um ponteiro de novo 
+         return 3;
          }
    return 0;
 }
 
 
 
-void main(){
+void main()
+{
 
 struct terreno t[5];
 
 for(int i=0;i<=4;i++){
     pegaTerreno(&t[i]);
-}
+    }
+
+int quad=0, ret=0;
 
 for(int j=0;j<=4;j++){
     TerrenoQuadeRet(&t[j]); 
-}
+    //if(TerrenoQuadeRet(&t[j])==2) quad++;
+    //if(TerrenoQuadeRet(&t[j])==3) ret++;
+    }
+int *VeTQ, *VetR;    
+
+VeTQ = (int*)malloc(quad * sizeof(int));
+VetR = (int*)malloc(ret * sizeof(int));
+
+/*for(int j=0;j<=4;j++){
+    TerrenoQuadeRet(&t[j]); 
+    if(TerrenoQuadeRet(&t[j])==2) VeTQ[j];
+    if(TerrenoQuadeRet(&t[j])==3) ret++;
+    }
 
 
-printf("%ls",peri);
+printf("%d",VeTQ[0]);*/
+printf("Base: %d Altura: %d Maior Perimetro: %d\n",bas,alt,peri);
 
 }
