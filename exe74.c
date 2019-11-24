66db4a5fe66a6c88f2010ded8f8854ac3ada2fc1
Voulth <55609484+Voulth@users.noreply.github.com>
2019-11-24 19:57:34 -0300

    Completo Só arrumar a parte do mais e menos quadrado valores

--- exe74.c
+++ exe74.c
@@ -10,17 +10,19 @@ para:
 #include <stdlib.h>
 
 int peri=0, bas=0, alt=0, identificador=0;//Variaveis não locais, para trabalhar com o calculo do Perimetro
-
+int quad=0, retan=0;
+int menQ=0, maiQ=1000000,idMa,idMe;
 struct terreno{//Terreno tem base e altura e angulos internos
 int base, altura, angulo, IDT;//Caracteristicas do Terreno + o seu numero identificador 
 
 };
 
 int perimetro(struct terreno *t){//Calculo do Perimetro do Terreno
-    int *per;//Ponteiro que ira apontar o local do perimetro salvo
-    *per = 2*(t->base+t->altura);//Calcula perimetro
-    if(*per>=peri){ //Associa o maior perimetro a variavel global peri 
-        peri = *per;
+    int per=0;//Ponteiro que ira apontar o local do perimetro salvo
+    per = 2*(t->base+t->altura);//Calcula perimetro
+    if(per>=peri){ //Associa o maior perimetro a variavel global peri 
+        peri = per;
+        
         return 1;
     }
     return 0;
@@ -34,33 +36,64 @@ scanf("%d %d %d %d",&t->altura,&t->base, &t->angulo, &t->IDT);
 int TerrenoQuadeRet(struct terreno *t){//Função para ver se um Terreno é quadrado ou retangulo
     if(t->altura == t->base && t->angulo == 90){//Testa se é um quadrado
          printf("O Terreno  de numero %d e quadrado\n",t->IDT);// Printa o identificador do terrno
-         //Erro perimetro 
+         quad++;
+         
          if(perimetro(t)==1){// Se já disse que a função iria receber um ponteiro eu não preciso dizer que ela é um ponteiro de novo 
          //Associa os valores do meu terreno atual para as variaveis globais para serem realizados os testes 
-            printf("Entrei\n");
              bas = t->base;
              alt = t->altura;
              identificador = t->IDT;
             }
-         //return 2;//Isso é usado para formar o vetor de lista dos terrenos quadrados 
+         return 1;
          }
     if(t->altura != t->base && t->angulo == 90){
          printf("O Terreno  de numero %d e retangulo\n",t->IDT);
+         retan++;
+         
          if(perimetro(t)==1){// Se já disse que a função iria receber um ponteiro eu não preciso dizer que ela é um ponteiro de novo 
          //Associa os valores do meu terreno atual para as variaveis globais para serem realizados os testes
              bas = t->base;
              alt = t->altura;
              identificador = t->IDT;
             }
-         //return 3;//Isso é usado para formar o vetor de lista dos terrenos retangulares
+         return 2;
          }
    //return 0;
 }
 
-//void maisQ(){
- //Função que ira receber um vetor struct de quadrados e ver quais mais se assemelham a um e menos se assemelham a um
+int Ret_Quad(struct terreno t){
+if(t.altura == t.base && t.angulo == 90){
+    return 1;
+    }
+    else{
+        return 0;
+    }
+}
 
-//}
+void maisQ(struct terreno *t){
+ //Função que ira receber um vetor struct de quadrados e ver quais mais se assemelham a um e menos se assemelham a um
+int dif=0;
+dif=t->base-t->altura;
+//Checo para ver se se aproxima de um quadrado ideal 1x1
+if(dif==0){
+    
+    if((t->base-1)<=maiQ){
+        maiQ=t->base-1;
+        idMa=t->IDT;
+
+    }
+
+    if((t->base-1)>=menQ){
+        
+        menQ=t->base-1;
+        idMa=t->IDT;
+
+    }
+}
+if(dif!=0)printf("O Terreno não é quadrado");
+
+
+}
 
 
 void main()
@@ -68,22 +101,53 @@ void main()
 int tamanho;
 printf("Insira o Tamanho de quantos terrenos tem de ter no vetor \n");
 scanf("%d",&tamanho);
-struct terreno t[3];
+struct terreno t[tamanho];
 
 
-for(int i=0;i<2;i++){
+for(int i=0;i<tamanho;i++){
     pegaTerreno(&t[i]);
     }
-for(int j=0;j<2;j++){
-    printf("Começei o for\n");
+for(int j=0;j<tamanho;j++){
+    
     TerrenoQuadeRet(&t[j]);
     
-    printf("Acabei o for\n");
+    
     }
 
-printf("Terreno %d Base: %d Altura: %d Maior Perimetro: %d\n",identificador,bas,alt,peri);
+struct terreno *q = malloc(quad*sizeof(q));
 
-
-
-
+int NQ=0;
+for (int k=0;k<tamanho;k++){
+    if(Ret_Quad(t[k])==1){
+        q[k-NQ]=t[k];
+        printf("KQ:%d\n",t[k].IDT);
+    
+        printf("Valor de Q: %d\n",q[k].IDT);
+    }
+    if(Ret_Quad(t[k])==0){
+        NQ++;
+    }
+    
 }
+
+
+for(int l=0;l<quad;l++){
+
+    printf("Q:%d\n",q[l].IDT);
+    
+}
+
+for(int l=0;l<quad;l++){
+
+    maisQ(&q[l]);
+    
+}
+
+
+
+printf("Terreno %d Base: %d Altura: %d Maior Perimetro: %d\n",identificador,bas,alt,peri);
+printf("Mais Quadrado: %d Menos Quadrado: %d",idMa,idMe);
+
+free(q);
+}
+
